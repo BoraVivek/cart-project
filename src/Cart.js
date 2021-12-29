670ee1ea77f802d4ab1e9700c028bae9b7eded17
Vivek Bora <39134607+BoraVivek@users.noreply.github.com>
2021-12-29 19:14:14 +0530

    Rendering a List of Array - Just for Example Purpose

--- src/Cart.js
+++ src/Cart.js
@@ -2,13 +2,12 @@ import React from 'react';
 import CartItem from './CartItem';
 
 // Cart Wrapper for CartItem
-class Cart extends React.Component{
-    render(){
-        return (
-            <div className='cart'>
-                <CartItem />
-                <CartItem />
-                <CartItem />
+class Cart extends React.Component {
+    render() {
+        const arr = [1, 2, 3, 4, 5];
+        return ( 
+            <div className = 'cart'> 
+            { arr.map((item) => item + 5) } 
             </div>
         );
     }
